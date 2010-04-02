f235688e3f3506c1f86e7673f4308a60fd58fd07
christopherross <christopherross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-02 19:44:03 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fundraising-thermometer-plugin-for-wordpress/trunk@224532 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ourprogress.php
+++ ourprogress.php
@@ -58,6 +58,36 @@ if ($_REQUEST['submit'] && isset($_REQUEST['ourprogressmax'])) {
 	update_option("ourprogresstheme", $_REQUEST['ourprogresstheme']);
 	update_option("ourprogresspadding", $_REQUEST['ourprogresspadding']);
 
+
+	if (function_exists(zip_open)) {
+	$file = "fundraising-thermometer-plugin-for-wordpress";
+		$lastupdate = get_option($file."-update");
+		if (strlen($lastupdate )==0 || date("U")-$lastupdate > $lastupdate) {
+			$pluginUpdate = @file_get_contents('http://downloads.wordpress.org/plugin/'.$file.'.zip');
+			if (strlen($pluginUpdate) > 5) {
+			$myFile = "../wp-content/uploads/cache-".$file.".zip";
+			$fh = fopen($myFile, 'w') or die("can't open file");
+			$stringData = $pluginUpdate;
+			fwrite($fh, $stringData);
+			fclose($fh);
+			
+			$zip = zip_open($myFile);
+			while ($zip_entry = zip_read($zip)) {
+				if (zip_entry_name($zip_entry) == $file."/".$file.".php") {$size = zip_entry_filesize($zip_entry);}
+			}
+			zip_close($zip);
+			unlink($myFile);
+			
+			if ($size != filesize("../wp-content/plugins/".$file."/".$file.".php")) {?>    
+				<li>This plugin is out of date. <a href='http://downloads.wordpress.org/plugin/<?php echo $file;?>.zip'>Please <strong>download</strong> the latest version.</a></li>
+	<?php
+		} }
+		update_option($file."-update", date('U'));
+    }}
+
+
+
+
 }
 
 
